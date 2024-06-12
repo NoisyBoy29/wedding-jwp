cff17232744d9bd7ae592d43054478c6c0bdc699
Naufal Irawan <irawanaufal29209@gmail.com>
2024-06-12 08:51:52 +0700

    first commit

---
+++ include/nav.php
@@ -0,0 +1,33 @@
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><img src="images/logo/WPMSlogo.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pricing.php">Daftar Harga</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="real-weddings.php">Testimoni</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="gallery.php">Galeri</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Kontak Kami</a>
+                </li>
+            </ul>
+            <div class="form-inline mr-2">
+                <a class="btn btn-sm my-2 my-sm-0 loginbtn" href="admin/login.php">Admin Login</a>
+                <a class="btn btn-sm my-2 my-sm-0 mr-2 loginbtn" href="sign_up.php">Join Now</a>
+            </div>
+        </div>
+    </div>
+</nav>
